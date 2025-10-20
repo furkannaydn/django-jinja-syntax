6050ae2a156d37a3f88ff4e98bcc7193ead033c4
furkan aydın <businessfurkan1@gmail.com>
2025-10-20 16:51:02 +0300

    İlk commit

---
+++ django-templates-project-main/template_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
+
+
+# Create your views here.
+
+def index(request):
+    return render(request,"template_app/first.html" )
+
+def weather(request):
+    weather_dictonary = {
+        "city":"Istanbul",
+        "temperature":30,
+        "condition":"Sunny",
+        "paris":[20,12,65,34],
+        "rome":{"morning":22,"noon":28,"night":18}
+    }
+    return render(request,"template_app/weather.html", context=weather_dictonary)
